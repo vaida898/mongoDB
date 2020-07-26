ca940c51ccc462931f06d2613440b274f41d559c
vaida898 <freelpfm@gmail.com>
2020-07-26 18:02:19 +0000

    added menu

---
+++ mongo_project.py
@@ -0,0 +1,52 @@
+import os 
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myTestDB"
+COLLECTION = "myFirstMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def show_menu():
+    print("")
+    print("1. Add record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+
+    option = input("Enter option: ")
+    return option
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("you have selected option 1")
+        elif option == "2":
+            print("you have selected option 2")
+        elif option == "3":
+            print("you have selected option 3")
+        elif option == "4":
+            print("you have selected option 4")
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+main_loop()
